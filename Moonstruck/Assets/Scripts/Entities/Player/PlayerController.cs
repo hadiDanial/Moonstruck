810e53b9305af0dcb81387737c93421349a95fcb
hadiDanial <hadidanial97@gmail.com>
2020-11-07 11:35:29 +0200

    Added PlayerController
    The player controller parses input and gives commands to the Entity for movement

---
+++ Moonstruck/Assets/Scripts/Entities/Player/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerController : Entity
+{
+    PlayerInput inputActions;
+    internal override void Awake()
+    {
+        base.Awake();        
+    }
+
+    public void OnMovement(InputValue value)
+    {
+        input = value.Get<Vector2>();
+        movementVector = useGravity ? new Vector2(input.x, 0).normalized : input;
+    }
+    public void OnJump()
+    {
+        Jump();
+    }
+
+
+}
