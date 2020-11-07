8265e32da005a6b07490f4c55dc322d403529291
hadiDanial <hadidanial97@gmail.com>
2020-11-07 11:34:16 +0200

    Update Entity.cs - movement and jumping
    Added Rigidbody2D movement and jumping to Entity.cs:
    - If the Entity is Active, it can move.
      Control in the air is based on a percentage
    - Gravity scale is based on the current GravityState, and whether the object is grounded or not. If it's grounded, the gravity scale is set to 0, otherwise to whatever the current gravity state is.
    - Jumping can only happen if the player is: Grounded, or the jump grace period hasn't ended yet (ie. Coyote Time)

--- Moonstruck/Assets/Scripts/Entities/Entity.cs
+++ Moonstruck/Assets/Scripts/Entities/Entity.cs
@@ -6,22 +6,42 @@ using UnityEngine;
 [RequireComponent(typeof(GravityObject))]
 public class Entity : MonoBehaviour
 {
+    [Header("Movement")]
+    [SerializeField] protected float movementSpeed;
+    [SerializeField] protected float jumpForce;
+    [SerializeField, Range(0, 0.4f)] protected float jumpGracePeriod = 0.1f;
+    [SerializeField, Range(0, 2)] protected float airControlPercent = 0.45f;
+    [SerializeField] protected bool useGravity;
+
     [Header("Health")]
-    [SerializeField] internal int maxHealth;
-    [SerializeField] internal int currentHealth { get; set; }
+    [SerializeField] protected int maxHealth;
+    [SerializeField] internal int currentHealth;
     [SerializeField] internal EntityState currentEntityState = EntityState.Active;
 
     [Header("Ground Check")]
-    [SerializeField] internal GameObject groundCheckLeft, groundCheckRight;
-    [SerializeField] internal LayerMask groundMask, enemyMask;
+    [SerializeField] protected GameObject groundCheckLeft;
+    [SerializeField] protected GameObject groundCheckRight;
+    [SerializeField] protected LayerMask groundMask, enemyMask;
+    internal float groundCheckDistance = 0.1f;
+    internal int internalSpeedMultiplier = 1000;
 
 
     internal Rigidbody2D rb;
+    internal Collider2D col;
     internal GravityObject gravityObj;
     internal Transform initialTransform;
     internal EntityState initialState;
-    internal bool canMove, canTakeDamage;
+    internal Vector2 input, movementVector;
+    internal bool canMove; // Might remove later, useless now
+    internal bool canTakeDamage;
+
+    internal float currentMovementMultiplier;
+    internal float jumpTimeElapsed;
     internal bool isGrounded => CheckIsGrounded();
+    internal bool canJump => isGrounded || jumpTimeElapsed >= 0;
+    internal bool isCollided, hasJumped;
+    internal float totalSpeedMultiplier => movementSpeed * currentMovementMultiplier * internalSpeedMultiplier;
+
     internal virtual void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -29,20 +49,60 @@ public class Entity : MonoBehaviour
         SetupEntity();
     }
 
+    internal virtual void Update()
+    {
+        if (IsActive())
+        {
+            jumpTimeElapsed = isGrounded ? jumpGracePeriod : jumpTimeElapsed - Time.deltaTime;
+            rb.gravityScale = (isGrounded && !hasJumped) || (isGrounded && isCollided) ? 0 : gravityObj.GetCurrentGravity();
+            if (input != Vector2.zero)
+            {
+                currentMovementMultiplier = isGrounded ? 1 : airControlPercent;
+                rb.AddForce(movementVector * totalSpeedMultiplier * Time.deltaTime, ForceMode2D.Force);
+            }
+            else
+            {
+                if (rb.velocity != Vector2.zero)
+                    rb.velocity = new Vector2(0.7f * rb.velocity.x, rb.velocity.y);
+            }
+        }
+
+
+    }
+
+    internal virtual void Jump()
+    {
+        if (IsActive() && canJump && !hasJumped ) 
+        {
+            hasJumped = true;
+            rb.AddForce(Vector2.up * jumpForce * 10, ForceMode2D.Impulse);
+        }
+    }
+
+
     internal virtual void SetupEntity()
     {
         currentHealth = maxHealth;
         initialTransform = transform;
         initialState = currentEntityState;
+        rb.velocity = Vector2.zero;
+        rb.angularVelocity = 0;
+        if (col != null) col.isTrigger = false;
+        if (!useGravity) gravityObj.SetGravityState(GravityState.Weightless);
+        rb.gravityScale = useGravity ? gravityObj.GetCurrentGravity() : 0;
         SetEntityState();
     }
 
+    /// <summary>
+    /// Resets the entity to its initial state: health, position, and EntityState
+    /// </summary>
     internal virtual void ResetEntity()
     {
         currentHealth = maxHealth;
         transform.position = initialTransform.position;
         transform.rotation = initialTransform.rotation;
         currentEntityState = initialState;
+        if (col != null) col.isTrigger = false;
         SetEntityState();
     }
 
@@ -53,19 +113,19 @@ public class Entity : MonoBehaviour
             case EntityState.Active:
                 {
                     canMove = true;
-                    canTakeDamage = false;
+                    SetVulnerable();
                 }
                 break;
             case EntityState.Inactive:
                 {
                     canMove = false;
-                    canTakeDamage = false;
+                    SetInvulnerable();
                 }
                 break;
             case EntityState.Dead:
                 {
                     canMove = false;
-                    canTakeDamage = false;
+                    SetInvulnerable();
                 }
                 break;
             default:
@@ -76,7 +136,22 @@ public class Entity : MonoBehaviour
         }
     }
 
-    internal virtual void Damage(int dmg)
+    /// <summary>
+    /// Returns true if the currentEntityState is Active
+    /// </summary>
+    internal bool IsActive()
+    {
+        return currentEntityState == EntityState.Active;
+    }
+
+    #region Health
+
+    /// <summary>
+    /// Damages the Entity and optionally sets it invulnerable for a time based on the hurt animation
+    /// </summary>
+    /// <param name="dmg"></param>
+    /// <param name="setInvulnerable"></param>
+    internal virtual void Damage(int dmg, bool setInvulnerable = true)
     {
         if (canTakeDamage)
         {
@@ -87,8 +162,13 @@ public class Entity : MonoBehaviour
                 Kill();
             }
             // TODO - Sound effect here
+            // TODO - SetInvulnerable() and Disable it via animation event
         }
     }
+    /// <summary>
+    /// Heals the Entity
+    /// </summary>
+    /// <param name="heal"></param>
     internal virtual void Heal(int heal)
     {
         currentHealth += heal;
@@ -96,11 +176,17 @@ public class Entity : MonoBehaviour
             currentHealth = maxHealth;
         // TODO - Sound effect here
     }
+    /// <summary>
+    /// Kills the Entity
+    /// </summary>
     internal virtual void Kill()
     {
         // TODO - Kill the Entity
+        currentEntityState = EntityState.Dead;
+        rb.velocity = Vector2.zero;
+        rb.gravityScale = gravityObj.GetNormalGravity();
+        if (col != null) col.isTrigger = true;
         // TODO - Sound effect here
-        throw new NotImplementedException();
     }
     internal virtual void SetInvulnerable()
     {
@@ -112,17 +198,46 @@ public class Entity : MonoBehaviour
         canTakeDamage = true;
     }
 
-    public bool CheckIsGrounded()
+    #endregion
+
+
+    /// <summary>
+    /// Returns true if the Entity is grounded. 
+    /// The entity is grounded if at least one of its ground checks returns true.
+    /// </summary>
+    internal bool CheckIsGrounded()
     {
         if (groundCheckLeft == null || groundCheckRight == null)
         {
             Debug.LogError(transform.name + ": No Ground Check Object!");
             return false;
         }
-        RaycastHit2D hitR = Physics2D.Raycast(groundCheckRight.transform.position, Vector2.down, 0.1f, groundMask);
-        RaycastHit2D hitL = Physics2D.Raycast(groundCheckLeft.transform.position, Vector2.down, 0.1f, groundMask);
-        if (hitR || hitL) return true;
+        RaycastHit2D hitR = Physics2D.Raycast(groundCheckRight.transform.position, Vector2.down, groundCheckDistance, groundMask);
+        RaycastHit2D hitL = Physics2D.Raycast(groundCheckLeft.transform.position, Vector2.down, groundCheckDistance, groundMask);
+        if (hitR || hitL)       
+            return true;     
         return false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isCollided = true;
+        if (isGrounded) hasJumped = false;
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        isCollided = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isCollided = false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(groundCheckRight.transform.position, groundCheckRight.transform.position + Vector3.down * groundCheckDistance);
+        Gizmos.DrawLine(groundCheckLeft.transform.position, groundCheckLeft.transform.position + Vector3.down * groundCheckDistance);
     }
 }
 
