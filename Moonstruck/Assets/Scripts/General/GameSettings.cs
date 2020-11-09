6e2328b33c03ee7eabe2fc878c9394efe82bc128
hadiDanial <hadidanial97@gmail.com>
2020-11-09 20:55:07 +0200

    Added GameSettings assets
    Added a basic settings asset that will eventually contain all of the game settings and configurations. For now, it only has basic options for aiming with the mouse and controller.

---
+++ Moonstruck/Assets/Scripts/General/GameSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Game Settings", menuName = "Moonstruck/General/New Game Settings")]
+public class GameSettings : ScriptableObject
+{
+    // Aiming
+    public float mouseSensitivity = 1;
+    public float gamepadAimSensitivity = 1;
+    public bool invertY = false;
+}
