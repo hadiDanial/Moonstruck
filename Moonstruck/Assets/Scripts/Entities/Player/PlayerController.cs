bdfbab35da905109619d549f25b62e643f154757
hadiDanial <hadidanial97@gmail.com>
2020-11-09 17:05:38 +0200

    Update PlayerController.cs

--- Moonstruck/Assets/Scripts/Entities/Player/PlayerController.cs
+++ Moonstruck/Assets/Scripts/Entities/Player/PlayerController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerController : Entity
 {
-    PlayerInput inputActions;
     internal override void Awake()
     {
         base.Awake();        
@@ -13,11 +12,15 @@ public class PlayerController : Entity
 
     public void OnMovement(InputValue value)
     {
+        if (!IsActive())
+            return;
         input = value.Get<Vector2>();
         movementVector = useGravity ? new Vector2(input.x, 0).normalized : input;
     }
     public void OnJump()
     {
+        if (!IsActive())
+            return;
         Jump();
     }
 
